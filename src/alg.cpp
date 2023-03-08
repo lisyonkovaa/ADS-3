cf3a2765f53b638bb64e676b068c6c15026fed45
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-03-08 18:35:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int result  = 0;
+    for (int i  =  0; i <= size; i++) {
+        if (arr[i] == value) {
+            result += 1;
+        }
+    }
+    return result;
 }
+
